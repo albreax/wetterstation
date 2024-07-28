34b95cb1a8874e33983f20d31ae6ddc3f541e126
Albrecht Naumann <albrecht.naumann@posteo.de>
2024-07-28 12:13:33 +0200

    wifi retry and poolServerName added

--- src/main.cpp
+++ src/main.cpp
@@ -4,53 +4,46 @@
 #include <GxEPD2_BW.h>
 #include <Adafruit_GFX.h>
 #include <SPI.h>
-#include "config.h" 
+#include "config.h"
 
 // NTP-Client
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 3600, 60000);
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000); // 3600 Sekunden = 1 Stunde
 
-// define the display
+// Define the display
 GxEPD2_BW<GxEPD2_420_GDEY042T81, GxEPD2_420_GDEY042T81::HEIGHT> display(GxEPD2_420_GDEY042T81(/*CS=*/ 10, /*DC=*/ 9, /*RST=*/ 8, /*BUSY=*/ 7));
 
-void setup() {
-  // initialize serial communication
-  Serial.begin(115200);
+void connectToWiFi() {
+  display.print("Connecting to WiFi...");
+  display.display();
   
-  // set up the SPI bus
-  SPI.begin(/*SCK=*/ 13, /*MISO=*/ -1, /*MOSI=*/ 11, /*SS=*/ 10);
-
-  // initialize the display
-  display.init(115200);
-
-  // setup WiFi
-  display.nextPage();
-  display.fillScreen(GxEPD_WHITE);
-  display.setCursor(0, 0);
-  display.setTextSize(2);
-  display.setTextColor(GxEPD_BLACK);
-  display.println("Connecting to WiFi...");
-  display.display();
-
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
 
+  int retries = 0;
+  while (WiFi.status() != WL_CONNECTED && retries < 20) {
+    delay(500);
+    Serial.print(".");
+    display.print(".");
+    display.display();
+    retries++;
   }
-  Serial.println("Connected to WiFi");
-  display.println("Connected to WiFi");
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nConnected to WiFi");
+    display.println("\nConnected to WiFi");
+  } else {
+    Serial.println("\nFailed to connect to WiFi");
+    display.println("\nFailed to connect to WiFi");
+  }
   display.display();
-  // NTP-Client initialise
-  timeClient.begin();
 }
 
-void loop() {
-  // fetch the time from the NTP-Server
+void displayTime() {
+  // Fetch the time from the NTP-Server
   timeClient.update();
 
   unsigned long epochTime = timeClient.getEpochTime();
-  struct tm *ptm = gmtime((time_t *)&epochTime); 
+  struct tm *ptm = gmtime((time_t *)&epochTime);
 
   ptm->tm_hour = (ptm->tm_hour + hourOffset) % 24;
 
@@ -60,17 +53,16 @@ void loop() {
   int hour = ptm->tm_hour;
   int minute = ptm->tm_min;
 
-  // format the date and time
+  // Format the date and time
   char dateBuffer[16];
   snprintf(dateBuffer, sizeof(dateBuffer), "%02d.%02d.%04d", day, month, year);
 
   char timeBuffer[16];
   snprintf(timeBuffer, sizeof(timeBuffer), "%02d:%02d", hour, minute);
 
-  // update the display
+  // Update the display
   display.setPartialWindow(0, 0, display.width(), display.height());
   display.firstPage();
-
   do {
     display.fillScreen(GxEPD_WHITE);
     display.setCursor(0, 0);
@@ -78,7 +70,40 @@ void loop() {
     display.setTextColor(GxEPD_BLACK);
     display.println(String(dateBuffer) + "              " + String(timeBuffer) + " Uhr");
   } while (display.nextPage());
+}
 
+void setup() {
+  // Initialize serial communication
+  Serial.begin(115200);
+
+  // Set up the SPI bus
+  SPI.begin(/*SCK=*/ 13, /*MISO=*/ -1, /*MOSI=*/ 11, /*SS=*/ 10);
+
+  // Initialize the display
+  display.init(115200);
+
+  // Setup WiFi
+  display.nextPage();
+  display.fillScreen(GxEPD_WHITE);
+  display.setCursor(0, 0);
+  display.setTextSize(2);
+  display.setTextColor(GxEPD_BLACK);
+  display.display();
+
+  connectToWiFi();
+
+  // Initialize NTP-Client
+  timeClient.begin();
+}
+
+void loop() {
+  // Ensure WiFi connection is active
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWiFi();
+  }
+  else {
+    displayTime();
+  }
   delay(1000);
 }
 
