d2a24441dfd5bdbf5605aeaacf512db3682321bb
Albrecht Naumann <albrecht.naumann@posteo.de>
2024-07-30 23:49:42 +0200

    mqtt ergänzt

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <GxEPD2_BW.h>
+#include <PubSubClient.h>
 #include <Adafruit_GFX.h>
 #include <SPI.h>
 #include "config.h"
@@ -10,14 +11,20 @@
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000); // 3600 Sekunden = 1 Stunde
 
+// Globale Variablen für MQTT
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 // Define the display
 GxEPD2_BW<GxEPD2_420_GDEY042T81, GxEPD2_420_GDEY042T81::HEIGHT> display(GxEPD2_420_GDEY042T81(/*CS=*/ 10, /*DC=*/ 9, /*RST=*/ 8, /*BUSY=*/ 7));
+
+String temperature = "N/A";
 
 void connectToWiFi() {
   display.print("Connecting to WiFi...");
   display.display();
   
-  WiFi.begin(ssid, password);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   int retries = 0;
   while (WiFi.status() != WL_CONNECTED && retries < 20) {
@@ -38,14 +45,31 @@ void connectToWiFi() {
   display.display();
 }
 
-void displayTime() {
+void mqtt_reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(MQTT_ID)) {
+      Serial.println("connected");
+      client.subscribe(MQTT_TOPIC);
+      Serial.print("Subscribed to topic: ");
+      Serial.println(MQTT_TOPIC);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+void displayContent() {
   // Fetch the time from the NTP-Server
   timeClient.update();
 
   unsigned long epochTime = timeClient.getEpochTime();
   struct tm *ptm = gmtime((time_t *)&epochTime);
 
-  ptm->tm_hour = (ptm->tm_hour + hourOffset) % 24;
+  ptm->tm_hour = (ptm->tm_hour + 1) % 24; // Adjust timezone offset if needed
 
   int day = ptm->tm_mday;
   int month = ptm->tm_mon + 1;
@@ -69,7 +93,23 @@ void displayTime() {
     display.setTextSize(2);
     display.setTextColor(GxEPD_BLACK);
     display.println(String(dateBuffer) + "              " + String(timeBuffer) + " Uhr");
+    display.println("Außentemperatur: " + temperature + "°C");
   } while (display.nextPage());
+}
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+  // Serial.print("Message arrived [");
+  // Serial.print(topic);
+  // Serial.print("] ");
+  // for (unsigned int i = 0; i < length; i++) {
+  //   Serial.print((char)payload[i]);
+  // }
+  // Serial.println();
+  // Hier können Sie die Nachrichtenverarbeitung hinzufügen
+  if (strcmp(topic, MQTT_TOPIC) == 0) {
+    // tempertur = "Außentemperatur: " + (char *)payload;
+    temperature = (char *)payload;
+  }
 }
 
 void setup() {
@@ -94,16 +134,25 @@ void setup() {
 
   // Initialize NTP-Client
   timeClient.begin();
+
+  // MQTT-Server setzen
+  client.setServer(MQTT_HOST, MQTT_PORT);
+  client.setCallback(mqtt_callback);
 }
 
 void loop() {
   // Ensure WiFi connection is active
   if (WiFi.status() != WL_CONNECTED) {
     connectToWiFi();
+  } else {
+    displayContent();
   }
-  else {
-    displayTime();
+
+  // Ensure MQTT client loop is called
+  if (!client.connected()) {
+    mqtt_reconnect();
   }
+  client.loop();
+
   delay(1000);
 }
-
